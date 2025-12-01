8c1f907131c589e2ce527d64087e233130cbe053
Gabriel Burgos <gabrielb102@gmail.com>
2025-11-30 22:11:01 -0800

    Remove Mobile / Tablet Banner

--- frontend/src/views/MapScreen.vue
+++ frontend/src/views/MapScreen.vue
@@ -2,15 +2,11 @@
 import Map from '@/components/map-ui/Map.vue'
 import MapSearch from '@/components/map-ui/MapSearch.vue'
 import Header from "@/components/Header.vue";
-import MobileMapBanner from '@/components/MobileMapBanner.vue'
 
 </script>
 
 <template>
-  <div class="size-full lg:hidden flex bg-gray-300 p-4 md:p-8">
-    <MobileMapBanner/>
-  </div>
-  <div class="size-full hidden lg:flex">
+  <div class="size-full flex">
     <div class="flex flex-col size-full">
       <Header>
         <template #center>
